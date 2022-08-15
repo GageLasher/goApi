5ba4aa0664132bd0906796d278fed83eec1c135a
gagelasher <rsgage21@gmail.com>
2022-08-15 14:51:08 -0600

    commit

--- main.go
+++ main.go
@@ -53,12 +53,23 @@ func getTodo(context *gin.Context) {
 	}
 	context.IndentedJSON(http.StatusOK, todo)
 }
+func setTodoComplete(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+	todo.Completed = !todo.Completed
+	context.IndentedJSON(http.StatusOK, todo)
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
-
+	router.PUT("/todos/:id", setTodoComplete)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
